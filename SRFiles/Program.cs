3cc545ceae978c8328740df493b04ed081bda846
zaharlol <kovtun.zahar12@mail.ru>
2023-04-13 15:03:25 +0300

    Добавьте файлы проекта.

---
+++ SRFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string NewFolder = @"D:/VS/SkillFactory/Новая папка";
+        if (Directory.Exists(NewFolder))
+        {
+            string[] dirs = Directory.GetDirectories(NewFolder);
+            foreach (string d in dirs)
+                Console.WriteLine(d);
+            Console.WriteLine();
+            Console.WriteLine("Файлы:");
+            string[] files = Directory.GetFiles(NewFolder);
+
+            foreach (string s in files)   
+                Console.WriteLine(s);
+        }
+
+        try
+        {
+                TimeSpan interval = TimeSpan.FromMinutes(30);
+                DirectoryInfo FI = new DirectoryInfo("D:/VS/SkillFactory/Новая папка");
+                foreach (FileInfo f in FI.GetFiles())
+                {
+                    f.Delete();
+                }
+                foreach (DirectoryInfo f in FI.GetDirectories())
+                {
+                    f.Delete();
+                }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Задание 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Linq.Expressions;
+using System.ComponentModel.Design.Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DirectoryInfo car = new DirectoryInfo (@"D:/VS/SkillFactory/Новая папка");
+        var folders = car.GetDirectories();
+        DI(folders); 
+    }
+
+public static void DI(DirectoryInfo[] folders)
+{
+        foreach (var folder in folders)
+        {
+            try
+            {
+                Console.WriteLine(folder.Name + $" - {Size(folder)}");
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+}
+public static long Size(DirectoryInfo d)
+{
+    long size = 0;
+    FileInfo[] fis = d.GetFiles();
+    foreach (FileInfo fi in fis)
+    {
+        size += fi.Length;
+    }
+    DirectoryInfo[] dis = d.GetDirectories();
+    foreach (DirectoryInfo di in dis)
+    {
+        size += Size(di);
+    }
+    return size;
+}
+}
