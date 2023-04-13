571c7905a045db8f4f6d24555a2a0f13ea22880a
zaharlol <kovtun.zahar12@mail.ru>
2023-04-13 15:53:14 +0300

    Готовый код

--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+            DirectoryInfo car = new DirectoryInfo(@"D:/VS/SkillFactory/Новая папка");
+            var folders = car.GetDirectories();
+            DI(folders);
+        DirectoryInfo FI = new DirectoryInfo("D:/VS/SkillFactory/Новая папка");
+        Console.WriteLine("Удалено: {0}", Size(car));
+        foreach (DirectoryInfo f in FI.GetDirectories())
+        {
+            f.Delete(true);
+        }
+        Console.WriteLine("Вес после отчистки: {0}", Size(car));
+    }
+    public static void DI(DirectoryInfo[] folders)
+    {
+        foreach (var folder in folders)
+        {
+            try
+            {
+                Console.WriteLine(folder.Name + $" : {Size(folder)}");
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    public static long Size(DirectoryInfo d)
+    {
+        long size = 0;
+        FileInfo[] fis = d.GetFiles();
+        foreach (FileInfo fi in fis)
+        {
+            size += fi.Length;
+        }
+        DirectoryInfo[] dis = d.GetDirectories();
+        foreach (DirectoryInfo di in dis)
+        {
+            size += Size(di);
+        }
+        return size;
+    }
+}
