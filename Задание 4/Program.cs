9c2557d7e8a76c69cf1f194d99b7a904918de09a
zaharlol <kovtun.zahar12@mail.ru>
2023-04-15 12:00:38 +0300

    Задание 4

---
+++ Задание 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string SettingsFileName = @"D:/VS/Новая папка/Students.bin";
+
+        if (File.Exists(SettingsFileName))
+        {
+            string StringValue;
+            using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+            {
+                StringValue = reader.ReadString();
+            }
+            Console.WriteLine("Из файла считано: {0}", StringValue);
+        }
+
+        Directory.CreateDirectory("C:/Users/denma/Desktop/Students");
+        string Di = ("C:/Users/denma/Desktop/Students");
+        string[] txtList = Directory.GetFiles(SettingsFileName, @"C:/D:/VS/Новая папка/Students.bin");
+        foreach (string f in txtList)
+        {
+            string fName = f.Substring(SettingsFileName.Length + 1);
+
+            try
+            {
+                File.Copy(Path.Combine(SettingsFileName, fName), Path.Combine(Di, fName));
+            }
+
+            catch (IOException copyError)
+            {
+                Console.WriteLine(copyError.Message);
+            }
+        }
+    }
+}
+[Serializable]
+class Contact
+{
+    public string Name { get; set; }
+    public string Group { get; set; }
+    public int DateOfBirth { get; set; }
+
+    public Contact(string name, string group, int dateofbirth)
+    {
+        Name = name;
+        Group = group;
+        DateOfBirth = dateofbirth;
+    }
+}
